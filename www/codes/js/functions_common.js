be615db7dab523946126759e0965d04cc4bae271
muscem <muscem1@hotmail.com>
2017-01-30 19:32:11 +0200

    test
    test

--- www/codes/js/functions_common.js
+++ www/codes/js/functions_common.js
@@ -164,31 +164,6 @@ var xmlsString = {
 function on_start(){
 	alert("cem1");
 	//document.addEventListener('deviceready', onDeviceReady, false);
-	
-	
-}
-
-$(document).ready(function() {
-    // are we running in native app or in a browser?
-    window.isphone = false;
-    if(document.URL.indexOf("http://") === -1 
-        && document.URL.indexOf("https://") === -1) {
-        window.isphone = true;
-    }
-
-    if( window.isphone ) {
-        document.addEventListener("deviceready", onDeviceReady, false);
-    } else {
-        onDeviceReady();
-    }
-});
-
-function onDeviceReady() {
-    // do everything here.
-	alert("device ready");
-}
-
-function onDeviceReady2(){
 	alert("cem2");
 	find_page_number();
 	alert("cem3");
